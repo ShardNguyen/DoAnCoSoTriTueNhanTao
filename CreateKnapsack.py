9c9ae10faa73db6b2ad07304c108a714ed64c7ec
Phù Thành Nhân <95023666+NoxionK@users.noreply.github.com>
2023-04-12 18:59:26 +0700

    Updated CreateKnapsack
    Updated the value generator with percentile

--- CreateKnapsack.py
+++ CreateKnapsack.py
@@ -7,19 +7,26 @@ import numpy as np  # For arrays
 
 # end
 
+
+# function
+def percent(part, whole):
+    return (part * whole) / 100.0
+
+
+# end
+
 i = rng.randint(1, 3)  # How many items are there
 W = rng.randint(100, 1000)  # Knapsack capacity
 
 m = rng.randint(1, 4)  # Number of classes
 
-value_range = range(1, W) #Need to use value_range to remove the chance of duplicating weight but different value
+value_range = range(1, W)  # Need to use value_range to remove the chance of duplicating weight but different value
 Wi = rng.sample(value_range, (10 - i))  # Weights
 
 
 vi = np.empty(10 - i, dtype=object)
 for x in range(10 - i):
-    vi[x] = np.random.randint(Wi[x] - 10, Wi[x] + 10)  # Values
-    # can change the value algorithm to be more inline with the weight of it
+    vi[x] = np.random.randint(Wi[x] - int(percent(Wi[x], 10)), Wi[x] + int(percent(Wi[x], 10))) # Values
 
 ci = np.random.randint(1, m + 1, 10 - i)  # Correspond classes
 
