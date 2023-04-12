d18b82e3c2c9a1472472acd74b982d873ed1037f
Phù Thành Nhân <95023666+NoxionK@users.noreply.github.com>
2023-04-12 17:05:23 +0700

    Added CreateKnapsack.py
    Use CreateKnapsack to easily create an input file with the necessary information about a Knapsack
    There is still some optimization needed

---
+++ CreateKnapsack.py
@@ -0,0 +1,63 @@
+# Phu Thanh Nhan
+# 21127382
+
+# Packages
+import random as rng  # For RNG
+import numpy as np  # For arrays
+
+# end
+
+i = rng.randint(1, 3)  # How many items are there
+W = rng.randint(100, 1000)  # Knapsack capacity
+
+m = rng.randint(1, 4)  # Number of classes
+
+value_range = range(1, W) #Need to use value_range to remove the chance of duplicating weight but different value
+Wi = rng.sample(value_range, (10 - i))  # Weights
+
+
+vi = np.empty(10 - i, dtype=object)
+for x in range(10 - i):
+    vi[x] = np.random.randint(Wi[x] - 10, Wi[x] + 10)  # Values
+    # can change the value algorithm to be more inline with the weight of it
+
+ci = np.random.randint(1, m + 1, 10 - i)  # Correspond classes
+
+
+file = open("Input.txt", "w")
+# Open file under 'w' write mode
+
+file.write(repr(W) + "\n")
+file.write(repr(m) + "\n")
+
+a = 0
+for x in Wi:
+    file.write(repr(x))
+    if a >= (len(Wi) - 1):
+        break
+    file.write(", ")
+    a += 1
+
+file.write("\n")
+
+a = 0
+for x in vi:
+    file.write(repr(x))
+    if a >= (len(vi) - 1):
+        break
+    file.write(", ")
+    a += 1
+file.write("\n")
+
+a = 0
+for x in ci:
+    file.write(repr(x))
+    if a >= (len(ci) - 1):
+        break
+    file.write(", ")
+    a += 1
+file.write("\n")
+
+
+file.close()
+# Close the file
